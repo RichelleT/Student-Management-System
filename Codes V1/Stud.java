cb14c589b5525df450e32294f6c79de479694995
Richelle Tan <r_kyj89@naver.com>
2019-05-17 20:09:40 +0800

    update

---
+++ Codes V1/Stud.java
@@ -0,0 +1,7 @@
+/*
+
+Written by Richelle Tan @ 17th May 2019.
+Code protected under copyright and is under the MIT license.
+Copyright 2019, Richelle Tan, All rights reserved.
+
+*/
---
+++ Codes V1/StudMSys.java
@@ -0,0 +1,13 @@
+/*
+
+Written by Richelle Tan @ 17th May 2019.
+Code protected under copyright and is under the MIT license.
+Copyright 2019, Richelle Tan, All rights reserved.
+
+*/
+
+public class StudMSys {
+	public static void main(String[] args){
+		
+	}
+}
