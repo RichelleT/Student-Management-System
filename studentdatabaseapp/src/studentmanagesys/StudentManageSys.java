0dfa5f89b3ceea8c68cf3e1048ccc39b97a829e6
Richelle Tan <r_kyj89@naver.com>
2019-05-29 01:07:33 +0800

    Update StudentManageSys.java

--- studentdatabaseapp/src/studentmanagesys/StudentManageSys.java
+++ studentdatabaseapp/src/studentmanagesys/StudentManageSys.java
@@ -13,24 +13,24 @@ import studentmanagesys.Student;
 
 public class StudentManageSys {
 	public static void main(String[] args){
-		
+
 		//ask how many users or students needed to add
-		
+
 		System.out.print("Enter number of students to enroll: ");
 		Scanner in = new Scanner(System.in);
 		int studNum = in.nextInt();
 		Student[] students = new Student[studNum];
-		
+
 		// create selected number of students
-		for (int n = 0; n < studNum; n++); {
+		for (int n = 0; n < studNum; n++) {
 			//print user-inputed student information
-			
+
 			students[n] = new Student();
 			students[n].enrollCourse();
 			students[n].payBal();
 		}
-		for (int n = 0; n < studNum; n++); {
+		for (int n = 0; n < studNum; n++) {
 		System.out.println(students[n].showStat());
 		}
 	}
-}	
+}
